af3911aeee2062ffe1708d710bfd18c1b25623f2
Uriel <uriel.avila95@gmail.com>
2017-10-06 05:43:20 -0500

    Paginas de editar y agregar acción agregadas
    Se agrego la estructura de las paginas de editar y agregar acción.

---
+++ src/pages/edit-actions/edit-actions.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditActionsPage } from './edit-actions';
+
+@NgModule({
+  declarations: [
+    EditActionsPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EditActionsPage),
+  ],
+})
+export class EditActionsPageModule {}
